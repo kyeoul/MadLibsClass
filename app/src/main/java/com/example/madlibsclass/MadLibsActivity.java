ea00ed1b3ff28c631254c15f5408ec789ef8cdc7
kyeoul <insertwinterhere@gmail.com>
2019-08-30 12:01:19 -0500

    much progress such wow code actually does something layout looks marginally better yay

--- app/src/main/java/com/example/madlibsclass/MadLibsActivity.java
+++ app/src/main/java/com/example/madlibsclass/MadLibsActivity.java
@@ -2,13 +2,23 @@ package com.example.madlibsclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MadLibsActivity extends AppCompatActivity {
+
+    public static final String MY_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mad_libs);
+
+        Intent intent = getIntent();
+        String myMessage = intent.getStringExtra(MY_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(myMessage);
+
     }
 }
--- app/src/main/java/com/example/madlibsclass/MainActivity.java
+++ app/src/main/java/com/example/madlibsclass/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.madlibsclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendText(View v){
+        ArrayList<EditText> editTextArray = new ArrayList<EditText>();
+        editTextArray.add((EditText) findViewById(R.id.playerEditText));
+        editTextArray.add((EditText) findViewById(R.id.adjEditText)) ;
+        editTextArray.add((EditText) findViewById(R.id.skillEditText));
+        editTextArray.add((EditText) findViewById(R.id.skillEditText2));
+        editTextArray.add((EditText) findViewById(R.id.nounEditText));
+        editTextArray.add((EditText) findViewById(R.id.playerEditText2));
+        editTextArray.add((EditText) findViewById(R.id.playerEditText3));
+
+        String myMessage = "Excuse me? " + editTextArray.get(0).toString() + " is bad? I'll have you know that ";
+        myMessage += editTextArray.get(0).getText().toString() + " is the most ";
+        myMessage += editTextArray.get(1).getText().toString() + " player in the whole school. His ";
+        myMessage += editTextArray.get(2).getText().toString() + " and ";
+        myMessage += editTextArray.get(3).getText().toString() + " is something we should be glad to witness. ";
+        myMessage += editTextArray.get(4).getText().toString() + "breaks the records. " + editTextArray.get(5).getText().toString() + " breaks the records. ";
+        myMessage += editTextArray.get(6).getText().toString() + " breaks the rules.";
+        myMessage += "You can keep your statistics. I prefer the magic.";
+
+        Intent intent = new Intent(this, MadLibsActivity.class);
+        intent.putExtra(MadLibsActivity.MY_MESSAGE, myMessage);
+        startActivity(intent);
+
     }
 }
